2f8f61b8e4e025ea2f92007a0674643d8c806ca7
Bradley  J. Spaulding <brad.spaulding@gmail.com>
2021-12-03 20:26:11 +0000

    day one part two

--- src/DayOne.hs
+++ src/DayOne.hs
@@ -4,12 +4,17 @@ main :: String -> IO ()
 main input = do
   let depths = readDepths $ lines input
   putStrLn $ show depths
-  let pairs = zip depths (drop 1 depths)
+  let windows = zip3 depths (drop 1 depths) (drop 2 depths)
+  let windowSums = map sumWindow windows
+  let pairs = zip windowSums (drop 1 windowSums)
   let increases = length $ filter isIncrease pairs
   putStrLn $ show increases
 
-isIncrease:: (Int, Int) -> Bool
+isIncrease :: (Int, Int) -> Bool
 isIncrease (x,y) = y > x
 
 readDepths :: [String] -> [Int]
 readDepths = map read
+
+sumWindow :: (Int, Int, Int) -> Int
+sumWindow (x, y, z) = x + y + z

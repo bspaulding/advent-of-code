2b22c9382740be40f50d0d412e45c720af51d239
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-12-04 19:58:09 -0800

    day three part two done

--- src/DayThree.hs
+++ src/DayThree.hs
@@ -4,7 +4,7 @@ main :: String -> IO ()
 main input = do
   let readings = map parseLine $ lines input
   putStrLn $ show readings
-  let cols = map (\i -> map (\xs -> xs !! i) readings) [0..(length (head readings) - 1)]
+  let cols = transpose readings
   putStrLn $ show cols
   let gammaBits = map calcGammaBit cols
   putStrLn $ show gammaBits
@@ -16,8 +16,36 @@ main input = do
   putStrLn $ show epsilon
   let power = gamma * epsilon
   putStrLn $ show power
-  let (oxygenBit, co2Bit) = calcRatingBits (head cols)
+  -- part two
+  let (oxygenBit, co2Bit) = calcRatingBits (cols !! 0)
   putStrLn $ show (oxygenBit, co2Bit)
+  let (oxygen, co2) = findRatings (readings, readings) 0
+  print (oxygen, co2)
+  let oxygenDec = interpBinary oxygen
+  let co2Dec = interpBinary co2
+  putStrLn $ "oxygen = " ++ show oxygenDec ++ ", co2 = " ++ show co2Dec ++ ", life support = " ++ show (oxygenDec * co2Dec)
+
+-- this is so gross, i tried to do both in a single recursive pass but that was dumb, now transposing and calcing bits twice anyway /shrug
+findRatings :: ([[Int]], [[Int]]) -> Int -> ([Int], [Int])
+findRatings (oxygenReadings, co2Readings) i = 
+  case (length oxygenReadingsLeft, length co2ReadingsLeft) of
+    (1, 1) -> (head oxygenReadingsLeft, head co2ReadingsLeft)
+    _ -> findRatings (oxygenReadingsLeft, co2ReadingsLeft) (i + 1)
+  where colsOxy = transpose oxygenReadings
+        colsCo2 = transpose co2Readings
+        (oxygenCrit, _) = calcRatingBits (colsOxy !! i)
+        (_, co2Crit) = calcRatingBits (colsCo2 !! i)
+        oxygenReadingsLeft = 
+          if length oxygenReadings == 1
+            then oxygenReadings
+            else filter (\x -> x !! i == oxygenCrit) oxygenReadings
+        co2ReadingsLeft = 
+          if length co2Readings == 1
+            then co2Readings
+            else filter (\x -> x !! i == co2Crit) co2Readings
+
+transpose readings = 
+  map (\i -> map (\xs -> xs !! i) readings) [0..(length (head readings) - 1)]
 
 interpBinary :: [Int] -> Int
 interpBinary bits' = x

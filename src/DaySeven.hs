71d0d6be1fd4535f3bb133c8c917d4bced00b467
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-12-07 15:37:01 -0800

    day seven part one

--- src/DaySeven.hs
+++ src/DaySeven.hs
@@ -1,5 +1,28 @@
 module DaySeven (main) where
 
+import qualified Data.List as List
+import qualified Data.Text as T
+
 main :: String -> IO ()
 main input = do
-    putStrLn "day seven"
+    let crabs = parse input
+    putStrLn $ "crabs = " ++ show crabs
+    let costs = map (\p -> (p, calcCost crabs p)) [0..(maxInList crabs)]
+    putStrLn $ "costs = " ++ show costs
+    let (position, minCost) = head $ List.sortBy (\a b -> if (snd a) > (snd b) then GT else LT) costs
+    putStrLn $ "min cost = " ++ show minCost ++ ", target position = " ++ show position
+
+calcCost :: [Int] -> Int -> Int
+calcCost positions targetPosition =
+    sum costs
+    where 
+        costs = [abs (targetPosition - position) | position <- positions]
+
+maxInList :: Ord a => [a] -> a
+maxInList [a] =  a
+maxInList (a : rest) = max a (maxInList rest)
+
+parse :: String -> [Int]
+parse s = map read ss
+    where
+        ss = map T.unpack $ T.splitOn (T.pack ",") (T.pack s)

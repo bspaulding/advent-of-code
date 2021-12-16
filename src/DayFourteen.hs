9eab505bbc29ab3a4fdadf33e4d6f0d872b4dbd6
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-12-15 16:15:11 -0800

    refactoring day fourteen

--- src/DayFourteen.hs
+++ src/DayFourteen.hs
@@ -12,28 +12,33 @@ main input = case parseInput input of
   Left err -> print err
   Right conf -> do
     print conf
-    let rules = insertionRules conf
-    let step10 = steps rules (polymerTemplate conf) 10
-    let counts = Map.toList $ freqs step10
-    let ((cmax, xmax), (cmin, xmin)) = (maximumBy (comparing snd) counts, minimumBy (comparing snd) counts)
-    print ((cmax, xmax), (cmin, xmin))
-    putStrLn $ "Difference = " ++ show (xmax - xmin)
+    doStep conf 10
+
+doStep :: Config -> Int -> IO ()
+doStep conf n = do
+  let rules = insertionRules conf
+  let stepN = steps rules (polymerTemplate conf) n
+  let counts = Map.toList $ freqs stepN
+  let ((cmax, xmax), (cmin, xmin)) = (maximumBy (comparing snd) counts, minimumBy (comparing snd) counts)
+  print ((cmax, xmax), (cmin, xmin))
+  let diff = xmax - xmin
+  putStrLn $ "difference at step " ++ show n ++ " = " ++ show diff
 
 freqs :: Ord a => [a] -> Map.Map a Int
 freqs = foldl getAndInc Map.empty
   where
     getAndInc acc x = Map.insert x (1 + Map.findWithDefault 0 x acc) acc
 
-steps :: Map.Map String Char -> String -> Int -> String
+steps :: InsertionRules -> String -> Int -> String
 steps rules cs n = foldl (\acc _ -> step rules acc) cs [0 .. (n - 1)]
 
-step :: Map.Map String Char -> String -> String
+step :: InsertionRules -> String -> String
 step rules cs = combined
   where
-    combined = foldl (\acc cs -> acc ++ drop 1 cs) (head inserted) (tail inserted)
-    inserted = map (insertion rules) (zip cs (drop 1 cs))
+    combined = foldl1 (\acc cs -> acc ++ drop 1 cs) inserted
+    inserted = zipWith (curry (insertion rules)) cs (drop 1 cs)
 
-insertion :: Map.Map String Char -> (Char, Char) -> String
+insertion :: InsertionRules -> (Char, Char) -> String
 insertion rules (c1, c2) =
   case Map.lookup [c1, c2] rules of
     Nothing -> [c1, c2]
@@ -41,7 +46,9 @@ insertion rules (c1, c2) =
 
 -- parsing
 
-data Config = Config {polymerTemplate :: String, insertionRules :: Map.Map String Char} deriving (Show)
+type InsertionRules = Map.Map String Char
+
+data Config = Config {polymerTemplate :: String, insertionRules :: InsertionRules} deriving (Show)
 
 parseInput :: String -> Either ParseError Config
 parseInput = parse inputParser "error"

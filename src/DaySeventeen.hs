d9e3478a1d1d3d03eea1de8a0e8ad0791aee6948
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-12-22 09:39:04 -0800

    day seventeen in progress

--- src/DaySeventeen.hs
+++ src/DaySeventeen.hs
@@ -1,5 +1,17 @@
 module DaySeventeen (main) where
 
+import qualified Data.List as List
+
 main :: String -> IO ()
 main input = do
-  putStrLn "DaySeventeen"
+  let ps = [(7, 2), (6, 3), (6, 8), (6, 9), (6, 10)]
+  putStrLn $ List.intercalate "\n" $ map (\p -> show p ++ " => " ++ show (endPos p)) ps
+
+triangle n = (n * (n + 1)) `div` 2
+
+endPos (vx, vy) = (tx, y, typos)
+  where
+    tx = triangle vx
+    y = typos - tyneg
+    typos = triangle vy
+    tyneg = triangle (vx - vy - 1)

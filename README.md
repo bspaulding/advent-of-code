02666544d41f165f0da01f02c8a06875408f63be
Bradley J. Spaulding <brad.spaulding@gmail.com>
2022-12-06 10:19:35 -0800

    removed stuff from 2021 work in main

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent of code
+
+years are in branches by year (2021, 2022, etc)
--- src/Utilities.hs
+++
@@ -1,9 +0,0 @@
-module Utilities where
-
-import qualified Data.Text as Text
-
-join :: String -> [String] -> String
-join sep xs = Text.unpack $ Text.intercalate (Text.pack sep) (map Text.pack xs)
-
-splitOn :: String -> String -> [String]
-splitOn sep s = map Text.unpack $ Text.splitOn (Text.pack sep) (Text.pack s)

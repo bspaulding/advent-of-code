366c8aecd9ede9bbb05fa797623c8dff8ed54150
Bradley J. Spaulding <brad.spaulding@gmail.com>
2022-12-07 16:47:25 -0800

    day7 tweaking

--- day7.hs
+++ day7.hs
@@ -15,7 +15,7 @@ main = do
     Just input -> do
       let inLines = filter (not .null) $ lines input
       putStrLn "Day 7"
-      let parsed = filter (not .isLs) $ parseTerminalOutput inLines
+      let parsed = filter (/= LS) $ parseTerminalOutput inLines
       print parsed
       -- let fs = scanl evalTermOutLine emptyFileTreeCursor parsed
       -- putStrLn $ unlines $ map (\(cmd, s) -> show cmd <> "\n" <> show s <> "\n" <> printTreeCursor s) (zip parsed (tail fs))
@@ -49,24 +49,8 @@ treeSize (Dir d children) = sum $ map treeSize children
 data FileTree = Dir String [FileTree] | File Int String
   deriving (Show, Eq)
 
-instance Ord FileTree where
-  compare (Dir d _ ) (Dir d' _) = compare d d'
-  compare (Dir d _ ) (File _ f) = compare d f
-  compare (File _ f) (Dir d _ )= compare d f
-  compare (File _ f) (File _ f')= compare f f'
-
-showTree :: Int -> FileTree -> String
-showTree indent (File size name) =
-  (map (const ' ') [0..indent*2]) <> "- " <> name <> " (file, size=" <> show size <> ")"
-showTree indent (Dir name children) =
-  (map (const ' ') [0..indent*2]) <> "- " <> name <> " (dir, size=" <> show (treeSize (Dir name children)) <> ")\n" <> unlines (map (showTree (indent + 1)) (List.sort children))
-
-printTree :: FileTree -> String
-printTree = showTree 0
-
 data FileTreeCursor = FileTreeCursor { tree :: FileTree, cd :: [String] }
   deriving (Show)
-printTreeCursor tc = "FileTreeCursor /" <> List.intercalate "/" (cd tc) <> "\n" <> printTree (tree tc)
 
 evalTermOutLine :: FileTreeCursor -> TermOutLine -> FileTreeCursor
 evalTermOutLine tc l =
@@ -93,11 +77,8 @@ isDirNamed :: String -> FileTree -> Bool
 isDirNamed dirName (File _ _ ) = False
 isDirNamed dirName (Dir dirName' _) = dirName == dirName'
 
-data TermOutLine = CD String | LS | DirD String | FileD Int String | Unknown String deriving (Show)
-
-isLs :: TermOutLine -> Bool
-isLs LS = True
-isLs _ = False
+data TermOutLine = CD String | LS | DirD String | FileD Int String | Unknown String
+  deriving (Eq, Show)
 
 parseTerminalOutput :: [String] -> [TermOutLine]
 parseTerminalOutput = map parseTermLine
@@ -112,7 +93,6 @@ parseTermLine l =
   where
     (fileSize, (_:fileName)) = span (not . isSpace) l
 
-
 getPuzzleInput :: IO (Maybe String)
 getPuzzleInput = do
   args <- getArgs
@@ -125,4 +105,20 @@ getPuzzleInput = do
     input <- readFile inPath
     putStrLn $ "---- Begin Puzzle Input ----\n" <> input <> "\n---- End Puzzle Input ----"
     return (Just input)
+
+-- stuff only related to pretty printing the trees
+
+instance Ord FileTree where
+  compare (Dir d _ ) (Dir d' _) = compare d d'
+  compare (Dir d _ ) (File _ f) = compare d f
+  compare (File _ f) (Dir d _ )= compare d f
+  compare (File _ f) (File _ f')= compare f f'
+
+printTree :: Int -> FileTree -> String
+printTree indent (File size name) =
+  (map (const ' ') [0..indent*2]) <> "- " <> name <> " (file, size=" <> show size <> ")"
+printTree indent (Dir name children) =
+  (map (const ' ') [0..indent*2]) <> "- " <> name <> " (dir, size=" <> show (treeSize (Dir name children)) <> ")\n" <> unlines (map (printTree (indent + 1)) (List.sort children))
+
+printTreeCursor tc = "FileTreeCursor /" <> List.intercalate "/" (cd tc) <> "\n" <> printTree 0 (tree tc)
 
